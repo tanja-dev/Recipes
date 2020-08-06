0ec6737c0e1ebbdb907bd2d9d69c63900b87ee46
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-08-06 23:51:56 +0200

    Create show action, route and view for the recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/recipes', to: 'recipes#index'
+  get '/recipes/:id', to: 'recipes#show'
 
   root to: 'recipes#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
