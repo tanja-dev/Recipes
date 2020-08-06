e8dc4386a9f7f92f0cf7dd6f5dd3de8bfab9463d
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-08-07 01:21:55 +0200

    Create new and create actions, new view, using form_with helper and get and post requests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,20 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    recipe.save
+    redirect_to recipe_path(recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/recipes', to: 'recipes#index'
+  get '/recipes/new', to: 'recipes#new', as: :new_recipe
   get '/recipes/:id', to: 'recipes#show', as: :recipe
+  post '/recipes', to: 'recipes#create'
 
   root to: 'recipes#index'
 
