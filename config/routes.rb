b1926f861ed599947851a5654f6ad84dc8aa8af7
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-08-07 02:00:50 +0200

    Create edit and update actions, edit view and get and patch requests for updating recipes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/recipes/new', to: 'recipes#new', as: :new_recipe
   get '/recipes/:id', to: 'recipes#show', as: :recipe
   post '/recipes', to: 'recipes#create'
+  get '/recipes/:id/edit', to: 'recipes#edit', as: :edit_recipe
+  patch '/recipes/:id', to: 'recipes#update' 
 
   root to: 'recipes#index'
 
