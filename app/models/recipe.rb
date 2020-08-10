d89b04f4e0c2625b6092bcd4bbda1109d160e050
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-08-10 18:16:22 +0200

    Style it, using Bulma framework

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+  has_many :instructions
+  has_many :ingredients
   validates :title, presence: true, length: { minimum: 4 }
   validates :description, presence: true, length: { minimum: 12 }
 end
