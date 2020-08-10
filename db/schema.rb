ef071a6a72bc2b7e9c71e01d41ed376618a67e72
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-08-10 13:34:14 +0200

    Create Instruction and Ingredient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_204433) do
+ActiveRecord::Schema.define(version: 2020_08_10_113019) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.text "body"
+    t.integer "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
+
+  create_table "instructions", force: :cascade do |t|
+    t.text "body"
+    t.integer "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_instructions_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -19,4 +35,6 @@ ActiveRecord::Schema.define(version: 2020_08_06_204433) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "ingredients", "recipes"
+  add_foreign_key "instructions", "recipes"
 end
