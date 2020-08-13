704103245f5ae571b77e97e3d73a1abea7fd54e6
Tanja Veljanovska <tanja.marathon@gmail.com>
2020-08-13 17:47:14 +0200

    Create controller, view and routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to: 'recipes#index'
+
   resources :recipes do
     resources :instructions
     resources :ingredients
   end
+
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
